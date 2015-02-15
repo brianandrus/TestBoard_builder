30426532bc3d682b0dd42cf86b578399db450677
Robotics <Robotics@CHS-ROBO-TEA>
2015-02-15 15:00:25 -0800

    Added Commands

--- TestBoard_builder/src/org/usfirst/frc2035/TestBoard_builder/commands/AirIn.java
+++ TestBoard_builder/src/org/usfirst/frc2035/TestBoard_builder/commands/AirIn.java
@@ -39,15 +39,17 @@ public class  AirIn extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.pneumatics.forward();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
--- TestBoard_builder/src/org/usfirst/frc2035/TestBoard_builder/commands/AirOut.java
+++ TestBoard_builder/src/org/usfirst/frc2035/TestBoard_builder/commands/AirOut.java
@@ -39,15 +39,17 @@ public class  AirOut extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.pneumatics.reverse();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
